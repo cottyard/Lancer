99d5bc40d361890a4af09113fe031ead49f4fa0e
cottyard <cottyard@gmail.com>
2022-06-07 00:49:10 +0800

    Delete Untitled-1.py

--- browser_prototype/src/Untitled-1.py
+++
@@ -1,9 +0,0 @@
-def decide_prime_number(n):
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
-
-for i in range(2, 100):
-    if decide_prime_number(i):
-        print(i)
