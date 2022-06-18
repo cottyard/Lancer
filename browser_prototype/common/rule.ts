26456e6731862bd4e8a073d572c5e2f78bd10694
cottyard <cottyard@gmail.com>
2022-06-18 11:32:55 +0800

    don't heat up friendly King

--- browser_prototype/common/rule.ts
+++ browser_prototype/common/rule.ts
@@ -188,6 +188,12 @@ export class Rule
         {
             for (let c of Rule.reachable_by(board, coord))
             {
+                let grid = board.at(c);
+                if (grid && grid.type() == King && grid.owner == unit.owner)
+                {
+                    // King cannot be defended, so don't heat up there.
+                    continue;
+                }
                 heat.at(c).heatup(unit.owner);
             }
         });
