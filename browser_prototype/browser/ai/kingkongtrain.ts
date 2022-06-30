7534f9fdb5fe0c193b611ded513ad6b989582f59
cottyard <cottyard@gmail.com>
2022-06-30 16:38:29 +0800

    Update kingkongtrain.ts

--- browser_prototype/browser/ai/kingkongtrain.ts
+++ browser_prototype/browser/ai/kingkongtrain.ts
@@ -26,8 +26,8 @@ const TrainingConfig: { [name: string]: ParamSearchConfig[] } = {
     {
       path: "reproduceRate",
       type: "number",
-      numSteps: 5,
-      step: 0.05,
+      numSteps: 2,
+      step: 0.1,
       min: 0,
       max: 1,
     },
@@ -36,10 +36,405 @@ const TrainingConfig: { [name: string]: ParamSearchConfig[] } = {
     {
       path: "surviveRate",
       type: "number",
-      numSteps: 3,
+      numSteps: 2,
       step: 0.05,
       min: 0,
       max: 1,
+    },
+  ],
+  keepOrderRate: [
+    {
+      path: "keepOrderRate",
+      type: "number",
+      numSteps: 2,
+      step: 0.05,
+      min: 0,
+      max: 1,
+    },
+  ],
+  mutationRate: [
+    {
+      path: "mutationRate",
+      type: "number",
+      numSteps: 2,
+      step: 0.05,
+      min: 0,
+      max: 1,
+    },
+  ],
+  evalCombos: [
+    {
+      path: "evalRate",
+      type: "number",
+      numSteps: 2,
+      step: 0.05,
+      min: 0,
+      max: 1,
+    },
+    {
+      path: "evalMode",
+      type: "string",
+      values: ["average", "min", "max"]
+    },
+  ],
+  skillValue: [
+    {
+      path: "skillValue",
+      type: "number",
+      numSteps: 2,
+      step: 1,
+      min: 0,
+      max: null,
+    },
+  ],
+  levelValueBase1: [
+    {
+      path: "levelValueBase.1",
+      type: "number",
+      numSteps: 2,
+      step: 2,
+      min: 0,
+      max: null,
+    },
+  ],
+  levelValueBase2: [
+    {
+      path: "levelValueBase.2",
+      type: "number",
+      numSteps: 2,
+      step: 4,
+      min: 0,
+      max: null,
+    },
+  ],
+  levelValueBase3: [
+    {
+      path: "levelValueBase.3",
+      type: "number",
+      numSteps: 2,
+      step: 6,
+      min: 0,
+      max: null,
+    },
+  ],
+  skillLevelMultiplier2: [
+    {
+      path: "skillLevelMultiplier.2",
+      type: "number",
+      numSteps: 2,
+      step: 0.1,
+      min: 0,
+      max: null,
+    },
+  ],
+  skillLevelMultiplier3: [
+    {
+      path: "skillLevelMultiplier.3",
+      type: "number",
+      numSteps: 2,
+      step: 0.1,
+      min: 0,
+      max: null,
+    },
+  ],
+  penaltyNoRetreatSkillInLast3Rows: [
+    {
+      path: "penaltyNoRetreatSkillInLast3Rows",
+      type: "number",
+      numSteps: 2,
+      step: 1,
+      min: null,
+      max: 0,
+    },
+  ],
+  valueToHalveWinRate: [
+    {
+      path: "valueToHalveWinRate",
+      type: "number",
+      numSteps: 2,
+      step: 10,
+      min: 20,
+      max: null,
+    },
+  ],
+  valueOfAdvancing1Row: [
+    {
+      path: "valueOfAdvancing1Row",
+      type: "number",
+      numSteps: 2,
+      step: 1,
+      min: 0,
+      max: null,
+    },
+  ],
+  valueOfAdvancing2Rows: [
+    {
+      path: "valueOfAdvancing2Rows",
+      type: "number",
+      numSteps: 2,
+      step: 1,
+      min: 0,
+      max: null,
+    },
+  ],
+  valueOfAdvancing3RowsOrMore: [
+    {
+      path: "valueOfAdvancing3RowsOrMore",
+      type: "number",
+      numSteps: 2,
+      step: 1,
+      min: 0,
+      max: null,
+    },
+  ],
+  edgePenalty: [
+    {
+      path: "edgePenalty",
+      type: "number",
+      numSteps: 2,
+      step: 1,
+      min: null,
+      max: 0,
+    },
+  ],
+  threatenAndDefendablePenalty: [
+    {
+      path: "threatenAndDefendablePenalty",
+      type: "number",
+      numSteps: 2,
+      step: 1,
+      min: null,
+      max: 0,
+    },
+  ],
+  threatenAndNotDefendablePenalty1: [
+    {
+      path: "threatenAndNotDefendablePenalty.1",
+      type: "number",
+      numSteps: 2,
+      step: 1,
+      min: null,
+      max: 0,
+    },
+  ],
+  threatenAndNotDefendablePenalty2: [
+    {
+      path: "threatenAndNotDefendablePenalty.2",
+      type: "number",
+      numSteps: 2,
+      step: 1,
+      min: null,
+      max: 0,
+    },
+  ],
+  threatenAndNotDefendablePenalty3: [
+    {
+      path: "threatenAndNotDefendablePenalty.3",
+      type: "number",
+      numSteps: 2,
+      step: 1,
+      min: null,
+      max: 0,
+    },
+  ],
+  supplyIncomeValue: [
+    {
+      path: "supplyIncomeValue",
+      type: "number",
+      numSteps: 2,
+      step: 1,
+      min: 0,
+      max: null,
+    },
+  ],
+  valueOfCapturingNeutralResources: [
+    {
+      path: "supplyIncomeValue",
+      type: "number",
+      numSteps: 2,
+      step: 1,
+      min: 0,
+      max: null,
+    },
+  ],
+  valueOfCapturingEnemyResources: [
+    {
+      path: "supplyIncomeValue",
+      type: "number",
+      numSteps: 2,
+      step: 1,
+      min: 0,
+      max: null,
+    },
+  ],
+  kingThreatenedAndDefendablePenalty: [
+    {
+      path: "kingThreatenedAndDefendablePenalty",
+      type: "number",
+      numSteps: 2,
+      step: 2,
+      min: null,
+      max: 0,
+    },
+  ],
+  kingThreatenedAndNotDefendableButMovablePenalty: [
+    {
+      path: "kingThreatenedAndNotDefendableButMovablePenalty",
+      type: "number",
+      numSteps: 2,
+      step: 2,
+      min: null,
+      max: 0,
+    },
+  ],
+  kingThreatenedAndNotDefendableNorMovablePenalty: [
+    {
+      path: "kingThreatenedAndNotDefendableNorMovablePenalty",
+      type: "number",
+      numSteps: 2,
+      step: 2,
+      min: null,
+      max: 0,
+    },
+  ],
+  kingNotMoveablePenalty: [
+    {
+      path: "kingNotMoveablePenalty",
+      type: "number",
+      numSteps: 2,
+      step: 2,
+      min: null,
+      max: 0,
+    },
+  ],
+  kingSurroundedByEnemyUnitPenalty1: [
+    {
+      path: "kingSurroundedByEnemyUnitPenalty.1",
+      type: "number",
+      numSteps: 2,
+      step: 1,
+      min: null,
+      max: 0,
+    },
+  ],
+  kingSurroundedByEnemyUnitPenalty2: [
+    {
+      path: "kingSurroundedByEnemyUnitPenalty.2",
+      type: "number",
+      numSteps: 2,
+      step: 1,
+      min: null,
+      max: 0,
+    },
+  ],
+  kingSurroundedByEnemyUnitPenalty3: [
+    {
+      path: "kingSurroundedByEnemyUnitPenalty.3",
+      type: "number",
+      numSteps: 2,
+      step: 1,
+      min: null,
+      max: 0,
+    },
+  ],
+  kingSurroundedByFriendlyUnitBonus1: [
+    {
+      path: "kingSurroundedByFriendlyUnitBonus.1",
+      type: "number",
+      numSteps: 2,
+      step: 1,
+      min: 0,
+      max: null,
+    },
+  ],
+  kingSurroundedByFriendlyUnitBonus2: [
+    {
+      path: "kingSurroundedByFriendlyUnitBonus.2",
+      type: "number",
+      numSteps: 2,
+      step: 1,
+      min: 0,
+      max: null,
+    },
+  ],
+  kingSurroundedByFriendlyUnitBonus3: [
+    {
+      path: "kingSurroundedByFriendlyUnitBonus.3",
+      type: "number",
+      numSteps: 2,
+      step: 1,
+      min: 0,
+      max: null,
+    },
+  ],
+  kingOnResourceSquareBonus: [
+    {
+      path: "kingOnResourceSquareBonus",
+      type: "number",
+      numSteps: 2,
+      step: 2,
+      min: 0,
+      max: null,
+    },
+  ],
+  kingNextMoveIsResourceSqaureBonus: [
+    {
+      path: "kingNextMoveIsResourceSqaureBonus",
+      type: "number",
+      numSteps: 2,
+      step: 1,
+      min: 0,
+      max: null,
+    },
+  ],
+  kingNextResourceMoveNotBlockedBonus: [
+    {
+      path: "kingNextResourceMoveNotBlockedBonus",
+      type: "number",
+      numSteps: 2,
+      step: 1,
+      min: 0,
+      max: null,
+    },
+  ],
+  kingCenterResourceSquareMultiplier: [
+    {
+      path: "kingCenterResourceSquareMultiplier",
+      type: "number",
+      numSteps: 2,
+      step: 0.25,
+      min: 0,
+      max: null,
+    },
+  ],
+  valueOfNonCentralGrid: [
+    {
+      path: "valueOfNonCentralGrid",
+      type: "number",
+      numSteps: 2,
+      step: 0.25,
+      min: 0,
+      max: null,
+    },
+  ],
+  valueOfCentralGrid: [
+    {
+      path: "valueOfCentralGrid",
+      type: "number",
+      numSteps: 2,
+      step: 0.25,
+      min: 0,
+      max: null,
+    },
+  ],
+  valueOfResourceGrid: [
+    {
+      path: "valueOfResourceGrid",
+      type: "number",
+      numSteps: 2,
+      step: 0.25,
+      min: 0,
+      max: null,
     },
   ],
 };
@@ -195,6 +590,7 @@ function pickGroup(state: TrainingState): TestGroup {
 
 function duel(param1: KingKongParams, param2: KingKongParams): GameStatus {
   var round = GameRound.new_game();
+  var numMoves = 0;
   while (round.status() == GameStatus.Ongoing) {
     const moves: Players<PlayerMove> = {
       [Player.P1]: new KingKong(param1).think(round, Player.P1, false),
